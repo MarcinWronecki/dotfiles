74927b86443ac0b126bbd5c19b198f1cf90d3c8a
Marcin Wronecki <2470703+MarcinWronecki@users.noreply.github.com>
2023-04-21 15:34:55 +0200

    add starship config and improve symlinks script

--- symlinks.sh
+++ symlinks.sh
@@ -6,7 +6,22 @@ SCRIPT_PATH="$(readlink -f "$0")"
 # Get the directory name of the script
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
+# Initialize a variable to keep track of whether any symbolic links were created
+links_created=false
+
 if [ ! -f ~/.gitconfig ]; then
-    echo "ln -sn ${SCRIPT_DIR}/.gitconfig ~/.gitconfig"
+    ln -sn "${SCRIPT_DIR}/.gitconfig" ~/.gitconfig
+    echo "Created symbolic link for .gitconfig"
+    links_created=true
+fi
+
+if [ ! -f ~/.config/starship.toml ]; then
+    ln -sn "${SCRIPT_DIR}/starship.toml" ~/.config/starship.toml
+    echo "Created symbolic link for starship.toml"
+    links_created=true
+fi
+
+if ! $links_created; then
+    echo "No symbolic links were created"
 fi
 
